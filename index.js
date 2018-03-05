844f31f3e79683ef99ce44924faf1738fd98b41f
alexandra-leveille <levian@hotmail.fr>
2018-03-05 16:47:45 +0100

    commit2

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 const express = require('express');
 const app = express();
 
+app.use('/', express.static(__dirname +'/front/build'));
 
 app.get('*', (req, res) => {
-  res.send('Hello CPROA');
+  //res.send('Hello CPROA');
+  res.sendFile(__dirname + '/front/build/index.html');
 });
 
 const port = process.env.PORT || 3000;
